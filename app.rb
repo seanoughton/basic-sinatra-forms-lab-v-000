395f3005ce044bb7961347f994d41eacfdaa2077
Sean Oughton <seanoughton@gmail.com>
2018-03-23 22:29:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,6 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/team' do
+    @name = params["name"]
     @coach = params["coach"]
     @point_guard = params["point_guard"]
     @shooting_guard = params["shooting_guard"]
